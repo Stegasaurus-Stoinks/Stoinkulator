7eb7ced4ab0ccb25a48eef68c7fc75d2226ca2b5
Wildape <pwilda100@gmail.com>
2023-03-30 22:59:17 -0700

    fuck tabs

--- MainStoinker/DataCollection/Test/livedatacollect3.py
+++ MainStoinker/DataCollection/Test/livedatacollect3.py
@@ -13,85 +13,86 @@ import time
 
 
 class IBapi(EWrapper, EClient):
-	def __init__(self):
-		EClient.__init__(self, self)
+    def __init__(self):
+        EClient.__init__(self, self)
+        
                 
-	def tickPrice(self, reqId, tickType, price, attrib):
-		if tickType == 2 and reqId == 1:
-			print('The current ask price is: ', price)
+    def tickPrice(self, reqId, tickType, price, attrib):
+        if tickType == 2 and reqId == 1:
+            print('The current ask price is: ', price)
 
-	def nextValidId(self, orderId: int):
-		print("Setting nextValidOrderId: %d", orderId)
-		self.nextValidOrderId = orderId
-		self.startData("AAPL")
+    def nextValidId(self, orderId: int):
+        print("Setting nextValidOrderId: %d", orderId)
+        self.nextValidOrderId = orderId
+        self.startData("AAPL")
 
-	def historicalData(self, reqId: int, bar: BarData):
-		# print("HistoricalData. ReqId:", reqId, "BarData.", bar)
+    def historicalData(self, reqId: int, bar: BarData):
+        # print("HistoricalData. ReqId:", reqId, "BarData.", bar)
 
-		candleData = [bar.date, bar.open, bar.high, bar.low, bar.close, bar.volume, bar.average]
-		self.df = self.df.append([candleData], ignore_index=True)
+        candleData = [bar.date, bar.open, bar.high, bar.low, bar.close, bar.volume, bar.average]
+        self.df = self.df.append([candleData], ignore_index=True)
 
-	# print(candleData)
-	# print(self.test)
-	# np.append(self.dataArray,[candleData])
+    # print(candleData)
+    # print(self.test)
+    # np.append(self.dataArray,[candleData])
 
-	def historicalDataEnd(self, reqId: int, start: str, end: str):
-		print("HistoricalDataEnd. ReqId:", reqId, "from", start, "to", end)
+    def historicalDataEnd(self, reqId: int, start: str, end: str):
+        print("HistoricalDataEnd. ReqId:", reqId, "from", start, "to", end)
 
-		print(self.df)
-		f = open('./MainStoinker/DataCollection/Test/datalive.csv', 'w+')
-		self.df.to_csv(f, index=False, header=False, lineterminator='\n')
-		f.close()
-		print("All Historical Data Collected: Live Data Starting Now...")
-		self.disconnect()
+        print(self.df)
+        f = open('./MainStoinker/DataCollection/Test/datalive.csv', 'w+')
+        self.df.to_csv(f, index=False, header=False, lineterminator='\n')
+        f.close()
+        print("All Historical Data Collected: Live Data Starting Now...")
+        self.disconnect()
 
-	# print("Finished")
+    # print("Finished")
 
-	def historicalDataUpdate(self, reqId: int, bar: BarData):
-		global lastbar
-		if lastbar != 0:
-			if (bar.average != lastbar.average):
-				print("HistoricalDataUpdate. ReqId:", reqId, "BarData.", bar)
+    def historicalDataUpdate(self, reqId: int, bar: BarData):
+        global lastbar
+        if lastbar != 0:
+            if (bar.average != lastbar.average):
+                print("HistoricalDataUpdate. ReqId:", reqId, "BarData.", bar)
 
-				# Open our existing CSV file in append mode
-				# Create a file object for this file
-				candleData = [bar.date, bar.open, bar.high, bar.low, bar.close, bar.volume, bar.average]
-				with open('./MainStoinker/DataCollection/Test/datalive.csv', 'a') as f_object:
-					# Pass this file object to csv.writer()
-					# and get a writer object
-					writer_object = writer(f_object)
+                # Open our existing CSV file in append mode
+                # Create a file object for this file
+                candleData = [bar.date, bar.open, bar.high, bar.low, bar.close, bar.volume, bar.average]
+                with open('./MainStoinker/DataCollection/Test/datalive.csv', 'a') as f_object:
+                    # Pass this file object to csv.writer()
+                    # and get a writer object
+                    writer_object = writer(f_object)
 
-					# Pass the list as an argument into
-					# the writerow()
-					writer_object.writerow(candleData)
+                    # Pass the list as an argument into
+                    # the writerow()
+                    writer_object.writerow(candleData)
 
-					# Close the file object
-					f_object.close()
+                    # Close the file object
+                    f_object.close()
 
-				lastbar = bar
+                lastbar = bar
 
-		else:
-			lastbar = bar
+        else:
+            lastbar = bar
 
-	def error(self, reqId, errorCode, errorString):
-		print("Error. Id: ", reqId, " Code: ", errorCode, " Msg: ", errorString)
+    def error(self, reqId, errorCode, errorString):
+        print("Error. Id: ", reqId, " Code: ", errorCode, " Msg: ", errorString)
 
-	def startData(self, ticker):
-		print("testy")
-		queryTime = (datetime.datetime.today() - datetime.timedelta(days=0)).strftime("%Y%m%d %H:%M:%S")
+    def startData(self, ticker):
+        print("testy")
+        queryTime = (datetime.datetime.today() - datetime.timedelta(days=0)).strftime("%Y%m%d %H:%M:%S")
 
-		contract = Contract()
-		contract.secType = "STK"
-		contract.symbol = ticker
-		contract.currency = "USD"
-		contract.exchange = "SMART"
+        contract = Contract()
+        contract.secType = "STK"
+        contract.symbol = ticker
+        contract.currency = "USD"
+        contract.exchange = "SMART"
 
-		self.reqHistoricalData(1, contract, "", "1 D", "1 min", "TRADES", 1, 1, True, [])
+        self.reqHistoricalData(1, contract, "", "1 D", "1 min", "TRADES", 1, 1, True, [])
 
-		f = open("./MainStoinker/DataCollection/Test/datalive.csv", "w")
-		f.truncate()
-		f.close()
+        f = open("./MainStoinker/DataCollection/Test/datalive.csv", "w")
+        f.truncate()
+        f.close()
 
-		self.df = pd.DataFrame()
-		self.dataArray = np.zeros(0)
-		self.test = 13
+        self.df = pd.DataFrame()
+        self.dataArray = np.zeros(0)
+        self.test = 13
