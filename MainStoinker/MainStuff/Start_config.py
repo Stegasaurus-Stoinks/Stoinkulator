4fdb2e1b52a9febf080c937756b5afb3ff239c6d
Wildape <wildape@rose-hulman.edu>
2024-07-02 11:28:17 -0700

    trendline algo sort of recreated,  attempt at offline dev feature loading offline data

--- MainStoinker/MainStuff/Start_config.py
+++ MainStoinker/MainStuff/Start_config.py
@@ -7,14 +7,14 @@ tickers = {}
 algos = []
 loggers = ["dum_dum"]
 log_level = logging.INFO
-LiveData = True
-LiveTrading = True
+LiveData = False
+LiveTrading = False
 #once trade excecution is figured out we should be able to get rid of one of these variables
 
 Debug = False
 
 #Frontend Variables
-FrontEndDisplay = True
+FrontEndDisplay = False
 FrontEndPort = '24.56.52.6:3000'
 updating = 1
 tickerIndex = 0
@@ -25,3 +25,7 @@ intraMinuteDisplay = True
 Duration = 2 #days
 StartDate = datetime.datetime.now() - relativedelta(month=0,weeks=0,day=Duration)
 TimeDelayPerPoint = 1.0 #seconds between backtested points
+
+#offline dev
+collectofflinedata = True
+offline = False
