66bdfaf2a7c56735c3cc233722d53bc6fb7b899d
Wildape <wildape@rose-hulman.edu>
2024-12-10 00:51:38 -0700

    dev branch testing

--- MainStoinker/MainStuff/Start_config.py
+++ MainStoinker/MainStuff/Start_config.py
@@ -15,7 +15,7 @@ Debug = False
 
 #Frontend Variables
 FrontEndDisplay = True
-FrontEndPort = '24.56.52.6:3000' # '192.168.1.108:3000' # 
+FrontEndPort = '24.56.52.6:3000' # '192.168.1.108:3000'  # 
 updating = 1
 tickerIndex = 0
 #LiveData Variables
