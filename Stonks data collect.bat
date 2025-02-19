5df47eae9a457cf8010bbe7bd1e9134dc2ddc2e8
STONK_PC <wildape@rose-hulman.edu>
2025-02-19 01:40:47 -0700

    added version numbers to requirements.txt and fixed .bat file for live trading

--- Stonks data collect.bat
+++ Stonks data collect.bat
@@ -1,4 +1,5 @@
+pushd %~dp0  & ::  added this line so that it runs correctly in task scheduler
 git pull
-venv\Scripts\python.exe -m pip install -r requirements.txt
-venv\Scripts\python.exe "MainStoinker\MainStuff\this is how we do it dum dum.py"
+.venv\Scripts\python.exe -m pip install -r requirements.txt
+.venv\Scripts\python.exe "MainStoinker\MainStuff\this is how we do it dum dum.py"
 pause
